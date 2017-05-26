a0397c5806f49c8dce7905e7d95ea54abe020126
侯亚迪 <houyadi@houyadidemacbook-pro.local>
2017-05-26 09:38:38 +0800

    Initial Commit

---
+++ UQIDDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UQIDDemo
+//
+//  Created by 侯亚迪 on 17/5/26.
+//  Copyright © 2017年 杭州魔品科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UQIDDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UQIDDemo
+//
+//  Created by 侯亚迪 on 17/5/26.
+//  Copyright © 2017年 杭州魔品科技. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
